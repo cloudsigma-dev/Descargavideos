d7d821888171ffd9733c2835e2eda466f959df56
joaquinito01 <joaquinito01@gmail.com>
2019-09-03 18:39:45 +0200

    Update a3borrado.php

--- secundario/a3borrado.php
+++ secundario/a3borrado.php
@@ -3,9 +3,29 @@
 class Atresplayer extends cadena{
 
 function calcula(){
-	header("HTTP/1.1 302 Found");
-	header("Location: http://descargavid.blogspot.com/2014/11/atresmedia-c-d.html");
-	exit;
+	$imagen = entre1y2($this->web_descargada, 'og:image" content="', '"');
+	$titulo = entre1y2($this->web_descargada, 'og:title" content="', '"');
+	$titulo = limpiaTitulo($titulo);
+	
+	$referer = 'aHR0cDovL2Rpc25leWNoYW5uZWwuZXM';
+	$url = entre1y2('http://gatekeeper.asociaciondevecinospoligononorte.tk/descargavideos/'. $this->web_descargada, '"url":"', '"');
+	dbug_($url);
+	$redir = CargaWebCurl($url, '', true, '', array(), false);
+	dbug_($redir);
+	$url = entre1y2($redir, 'Location: ', "\r");
+	$obtenido=array(
+		'titulo' => $titulo,
+		'imagen' => $imagen,
+		'enlaces' => array(
+			array(
+				//'url_txt' => 'Descargar',
+				'url' => $url,
+				'tipo' => 'm3u8'
+			)
+		)
+	);
+	$obtenido['alerta_especifica'] = 'No funciona con JDownloader.';
+	finalCadena($obtenido);
 }
 
 }
